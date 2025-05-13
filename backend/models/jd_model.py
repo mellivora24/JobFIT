2c9c0cbe014e3bf2890266c54c79b50516148c40
Quyết Thành <thanh139ptit@gmail.com>
2025-05-14 02:51:09 +0700

    fix (jd_model): Chinh sua cac truong bat buoc va khong bat buoc

--- backend/models/jd_model.py
+++ backend/models/jd_model.py
@@ -1,8 +1,7 @@
 # File này được sử dụng để tạo / ánh xạ các mô hình dữ liệu cho JD (Job Description) mà người dùng cung cấp.
 
-from datetime import date
 from typing import List, Optional
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 
 class _JobDescription(BaseModel):
     """
@@ -16,7 +15,7 @@ class _JobRequirement(BaseModel):
     Yêu cầu công việc mà JD cung cấp.
     """
     knowledge: Optional[str] = Field(None, description="Kiến thức")
-    skills: Optional[List[str]] = Field(None, description="Kỹ năng")
+    skills: List[str] = Field(..., description="Kỹ năng")
     experience: Optional[str] = Field(None, description="Kinh nghiệm làm việc")
     other_requirements: Optional[str] = Field(None, description="Yêu cầu khác (ví dụ: độ tuổi, giới tính, học vấn)")
 
@@ -27,7 +26,6 @@ class JD(BaseModel):
     - Yêu cầu công việc:
         + Kiên thức: yêu cầu về kiến thức mà JD cung cấp.
         + Kỹ năng: yêu cầu về kỹ năng mà JD cung cấp.
-        + Kinh nghiệm làm việc: yêu cầu về kinh nghiệm làm việc mà JD cung cấp.
         + Yêu cầu khác: yêu cầu khác mà JD cung cấp (ví dụ: yêu cầu về độ tuổi, giới tính, hoc vấn, v.v...).
     """
     job_description: _JobDescription = Field(..., description="Mô tả công việc")
--- backend/utils/embedded_vector.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-
-def embedded_vector(text):
-    return model.encode(text)
